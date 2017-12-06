59108f64665a0d667d730623999a53e75c8f05d6
byverdu <byverdu@gmail.com>
2017-12-06 17:00:52 +0000

    added test for returned props

--- __tests__/module.test.js
+++ __tests__/module.test.js
@@ -10,4 +10,26 @@ describe( 'Console-colors', () => {
   it( 'is a function', () => {
     expect( consColors ).to.be.a( 'function' );
   });
+
+  it( 'returns an Object', () => {
+    expect( typeof consColors()).to.eq( 'object' );
+  });
+
+  it( 'returns an Object with a "log" property that is a function', () => {
+    expect( consColors())
+      .to.have.property( 'log' )
+      .and.is.a( 'function' );
+  });
+
+  it( 'returns an Object with a "warn" property that is a function', () => {
+    expect( consColors())
+      .to.have.property( 'warn' )
+      .and.is.a( 'function' );
+  });
+
+  it( 'returns an Object with a "error" property that is a function', () => {
+    expect( consColors())
+      .to.have.property( 'error' )
+      .and.is.a( 'function' );
+  });
 });
